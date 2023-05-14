13632f21cff8719807a13115adf03fc9518644fa
lab1 <zhanerketkva7@gmail.com>
2023-05-14 18:14:42 +0600

    HashNode

---
+++ src/HashNode.java
@@ -0,0 +1,26 @@
+public class HashNode<K, V> {
+    private K key;
+    private V value;
+    public HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + key + " " + value + "}";
+    }
+
+}
