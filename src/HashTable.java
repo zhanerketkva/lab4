6e4764a08ba9a6627de174f1252f460c0cba8171
lab1 <zhanerketkva7@gmail.com>
2023-05-14 17:55:00 +0600

    hash table (remove)

--- src/HashTable.java
+++ src/HashTable.java
@@ -4,25 +4,29 @@ public class HashTable<K, V> {
     private LinkedList<HashNode<K, V>>[] chain;
     private int M = 11;
     private int size;
+
     public HashTable() {
         chain = new LinkedList[M];
         size = 0;
     }
+
     public HashTable(int m) {
         this.M = m;
         chain = new LinkedList[M];
         size = 0;
     }
+
     private int hash(K key) {
         return key.hashCode() % M;
     }
+
     public void put(K key, V value) {
         int index = hash(key);
-        if (chain[index] == null){
+        if (chain[index] == null) {
             chain[index] = new LinkedList<HashNode<K, V>>();
         }
-        for (HashNode<K, V> node: chain[index]){
-            if(node.getKey().equals(key)){
+        for (HashNode<K, V> node : chain[index]) {
+            if (node.getKey().equals(key)) {
                 node.setValue(value);
                 return;
             }
@@ -30,3 +34,18 @@ public class HashTable<K, V> {
         chain[index].add(new HashNode<K, V>(key, value));
         size++;
     }
+    public V remove(K key) {
+        int index = hash(key);
+        if (chain[index] == null) return null;
+
+        for (HashNode<K, V> node: chain[index]){
+            if(node.getKey().equals(key)) {
+                chain[index].remove(node);
+                size--;
+                return node.getValue();
+            }
+        }
+        size--;
+        return null;
+    }
+}
